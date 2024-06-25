cc5972ebad7ee9f8dd5863633b436bf5d612f8fd
jesus <jantolin@inhub.es>
2024-06-25 12:58:15 +0200

    Configuración inicial del proyecto Django

---
+++ cuanto_vale/cuanto_vale/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cuanto_vale project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cuanto_vale.settings')
+
+application = get_wsgi_application()
