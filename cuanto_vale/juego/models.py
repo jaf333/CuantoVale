fbc4ed90f77f05485cea46ddc89a25176e654026
jesus <jantolin@inhub.es>
2024-06-25 13:13:28 +0200

    Creación del modelo Jugador para almacenar datos de jugadores de fútbol

--- cuanto_vale/juego/models.py
+++ cuanto_vale/juego/models.py
@@ -1,3 +1,10 @@
+# En juego/models.py
 from django.db import models
 
-# Create your models here.
+class Jugador(models.Model):
+    nombre = models.CharField(max_length=100)
+    valor_mercado = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen_url = models.URLField()
+
+    def __str__(self):
+        return self.nombre
